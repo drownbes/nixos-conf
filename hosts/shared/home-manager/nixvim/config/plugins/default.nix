bdf48d0a9cdd78f8d57c40dabb311f542490d1a4
Artem Markov <drownbes@gmail.com>
2025-01-22 11:14:34 +0200

    fix plugins definition

--- hosts/shared/home-manager/nixvim/config/plugins/default.nix
+++ hosts/shared/home-manager/nixvim/config/plugins/default.nix
@@ -1,15 +1,17 @@
-{...}: {
-  programs.nixvim =
-    {}
-    // (import ./yanky.nix)
-    // (import ./toggleterm.nix)
-    // (import ./lualine.nix)
-    // (import ./telescope.nix)
-    // (import ./treesitter.nix)
-    // (import ./gitsigns.nix)
-    // (import ./trouble.nix)
-    // (import ./neo-tree.nix)
-    // (import ./cmp.nix)
-    // (import ./lsp.nix)
-    // (import ./nix-develop.nix);
+{lib, ...}: let 
+  recMerge = mlist: lib.lists.foldl lib.attrsets.recursiveUpdate {} mlist;
+in {
+  programs.nixvim = recMerge [
+    (import ./yanky.nix)
+    (import ./toggleterm.nix)
+    (import ./lualine.nix)
+    (import ./telescope.nix)
+    (import ./treesitter.nix)
+    (import ./gitsigns.nix)
+    (import ./trouble.nix)
+    (import ./neo-tree.nix)
+    (import ./cmp.nix)
+    (import ./lsp.nix)
+    (import ./nix-develop.nix)
+  ];
 }
