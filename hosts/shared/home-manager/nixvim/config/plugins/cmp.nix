404b85c1a0c4b210f32166eb2859ba9ce2b52d2e
Artem Markov <drownbes@gmail.com>
2025-01-23 16:57:03 +0200

    remap nvim cmp keys

--- hosts/shared/home-manager/nixvim/config/plugins/cmp.nix
+++ hosts/shared/home-manager/nixvim/config/plugins/cmp.nix
@@ -16,49 +16,61 @@
       {name = "buffer";}
       {name = "rg";}
     ];
+
+
     settings.mapping = {
-      "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-      "<C-f>" = "cmp.mapping.scroll_docs(4)";
-      "<C-Space>" = ''
-        cmp.mapping(function(fallback)
-          local luasnip = require("luasnip")
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" })
+      __raw = ''
+        cmp.mapping.preset.insert({
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        })
       '';
-      "<S-Tab>" = ''
-        cmp.mapping(function(fallback)
-        local luasnip = require("luasnip")
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" })
-      '';
-      "<CR>" = ''
-        cmp.mapping(function(fallback)
-          local luasnip = require("luasnip")
-          if cmp.visible() then
-              if luasnip.expandable() then
-                  luasnip.expand()
-              else
-                  cmp.confirm({
-                      select = true,
-                  })
-              end
-          else
-              fallback()
-          end
-        end)'';
     };
+
+    #settings.mapping = {
+    #  "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+    #  "<C-f>" = "cmp.mapping.scroll_docs(4)";
+    #  "<C-Space>" = ''
+    #    cmp.mapping(function(fallback)
+    #      local luasnip = require("luasnip")
+    #      if cmp.visible() then
+    #        cmp.select_next_item()
+    #      elseif luasnip.locally_jumpable(1) then
+    #        luasnip.jump(1)
+    #      else
+    #        fallback()
+    #      end
+    #    end, { "i", "s" })
+    #  '';
+    #  "<S-Tab>" = ''
+    #    cmp.mapping(function(fallback)
+    #    local luasnip = require("luasnip")
+    #      if cmp.visible() then
+    #        cmp.select_prev_item()
+    #      elseif luasnip.locally_jumpable(-1) then
+    #        luasnip.jump(-1)
+    #      else
+    #        fallback()
+    #      end
+    #    end, { "i", "s" })
+    #  '';
+    #  "<CR>" = ''
+    #    cmp.mapping({
+    #      i = function(fallback)
+    #       local luasnip = require("luasnip")
+    #       if cmp.visible() and cmp.get_active_entry() then
+    #          if luasnip.expandable() then
+    #              luasnip.expand()
+    #          else
+    #            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+    #          end
+    #       else
+    #         fallback()
+    #       end
+    #      end,
+    #      s = cmp.mapping.confirm({ select = true }),
+    #      c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+    #    })'';
+    #};
   };
 
   plugins.cmp-cmdline.enable = true;
